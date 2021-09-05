818349b64b7df63086570a3ec4da1a5002cadea4
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2021-09-05 16:29:07 +0530

    Add files via upload

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,39 @@
 import React from "react";
 import { Button, View, Dimensions, ScrollView } from "react-native";
 import Carosuel from "/Users/gauravtaparia/Desktop/Laundry/navigation/carosuel.js";
-import OrderButton from "../navigation/OrderButton";
+
+import Tabs from "../navigation/tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 const { width } = Dimensions.get("window");
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <ScrollView>
       {/* OfferWall Carosuel Component */}
       <View style={{ color: "#fff" }}>
         <Carosuel />
       </View>
+
       {/* Place your order button */}
-      <OrderButton />
+      <View style={{ backgroundColor: "#fff" }}>
+        <View
+          style={{
+            width: width * 0.5,
+            borderRadius: 20,
+            backgroundColor: "#e32f45",
+            marginLeft: width * 0.25,
+            marginRight: width * 0.25,
+            alignItems: "center",
+          }}
+        >
+          <Button
+            title="Book Your Pickup Now"
+            color="#fff"
+            onPress={() => navigation.navigate("Order")}
+          />
+        </View>
+      </View>
     </ScrollView>
   );
 }
