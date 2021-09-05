2b4029069cc661848af708d2caed1d53980a84a7
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2021-09-05 14:06:06 +0530

    Add files via upload

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { Button, View, Dimensions, ScrollView } from "react-native";
+import Carosuel from "/Users/gauravtaparia/Desktop/Laundry/navigation/carosuel.js";
+import OrderButton from "../navigation/OrderButton";
+
+const { width } = Dimensions.get("window");
 
 function HomeScreen() {
-  return <View></View>;
+  return (
+    <ScrollView>
+      {/* OfferWall Carosuel Component */}
+      <View style={{ color: "#fff" }}>
+        <Carosuel />
+      </View>
+      {/* Place your order button */}
+      <OrderButton />
+    </ScrollView>
+  );
 }
 
 export default HomeScreen;
