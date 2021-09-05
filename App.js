174f9db623b00f3f9ebb9c4ffb8c74a625a1f953
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2021-09-05 16:27:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -5,12 +5,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
 import Carosuel from "./navigation/carosuel";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import OrderScreen from "./screens/OrderScreen";
+import RewardScreen from "./screens/RewardScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import RecentOrderScreen from "./screens/RecentOrderScreen";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tabs />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Tabs"
+            component={Tabs}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Order"
+            component={OrderScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="PastOrders" component={RecentOrderScreen} />
+          <Stack.Screen name="Reward" component={RewardScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
