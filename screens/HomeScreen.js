729cef420c07a300d03780bd4da352c6060cecf9
Raahim08 <71621239+Raahim08@users.noreply.github.com>
2021-09-07 14:12:25 +0530

    Add files via upload

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Button,
   StyleSheet,
@@ -8,55 +8,90 @@ import {
   TouchableOpacity,
   Dimensions,
   ScrollView,
-} from "react-native";
-import Carosuel from "/Users/gauravtaparia/Desktop/Laundry/navigation/carosuel.js";
+  TextInput,
+  FlatList,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import * as Animatable from 'react-native-animatable'
+import Carosuel from '../navigation/Carosuel'
 
-import Membership from "../navigation/Membership";
-import Skillindia from "../navigation/Skillindia";
-import Ourservice from "../navigation/Ourservice";
-import Forquery from "../navigation/Forquery";
-import Card from "../navigation/Card";
-import Reviewcard from "../navigation/Reviewcard";
-import Referral from "../navigation/Referral";
+import Membership from '../navigation/Membership'
+import Skillindia from '../navigation/Skillindia'
+import Ourservice from '../navigation/Ourservice'
+import Forquery from '../navigation/Forquery'
+import Card from '../navigation/Card'
+import Reviewcard from '../navigation/Reviewcard'
+import Referral from '../navigation/Referral'
 
-const { width } = Dimensions.get("window");
-const height = width * 0.58;
+const { width } = Dimensions.get('window')
+const height = width * 0.58
 
 function HomeScreen({ navigation }) {
   return (
     <ScrollView>
+      {/* SearchBar */}
+      <View stle={{ flex: 1 }}>
+        <View
+          style={{
+            height: 80,
+            backgroundColor: '#000',
+            justifyContent: 'center',
+            paddingHorizontal: 10,
+            marginTop: width * 0.07,
+          }}
+        >
+          <Animatable.View
+            animation='slideInRight'
+            duration={500}
+            style={{
+              height: 50,
+              backgroundColor: '#fff',
+              flexDirection: 'row',
+              padding: 5,
+              alignItems: 'center',
+              borderRadius: 5,
+            }}
+          >
+            <Icon name='ios-search' style={{ fontSize: width * 0.04 }} />
+            <TextInput
+              placeholder='Search'
+              style={{ fontSize: width * 0.04, marginLeft: 15 }}
+            />
+          </Animatable.View>
+        </View>
+      </View>
       {/* OfferWall Carosuel Component */}
-      <View style={{ color: "#fff" }}>
+      <View style={{ color: '#fff' }}>
         <Carosuel />
       </View>
       {/* Place your order button */}
-      <View style={{ backgroundColor: "#fff" }}>
+      <View style={{ backgroundColor: '#fff' }}>
         <TouchableOpacity
-          onPress={() => navigation.navigate("Order")}
+          onPress={() => navigation.navigate('Order')}
           style={{
             width: width * 0.5,
             borderRadius: 20,
-            backgroundColor: "#e32f45",
+            backgroundColor: '#e32f45',
             marginLeft: width * 0.25,
             marginRight: width * 0.25,
-            alignItems: "center",
+            alignItems: 'center',
           }}
         >
           <Text
             style={{
               fontSize: width * 0.04,
-              fontWeight: "bold",
-              color: "white",
+              fontWeight: 'bold',
+              color: 'white',
               padding: 10,
+              textAlign: 'center',
             }}
           >
             Book Your Pickup Now
           </Text>
         </TouchableOpacity>
       </View>
-      <View style={{ color: "#fff" }}>
-        <Card />
-      </View>
+      {/*Top Rated In Your Area*/}
+      <Card />
       {/* Membership */}
       <Membership />
       {/* Our Service */}
@@ -70,8 +105,8 @@ function HomeScreen({ navigation }) {
       {/* Offer Image */}
       <View style={{ height: 100 }}>
         <Image
-          source={require("../assets/3.jpg")}
-          resizeMode="cover"
+          source={require('../assets/3.jpg')}
+          resizeMode='cover'
           style={{
             height: height * 0.6,
             width,
@@ -80,9 +115,9 @@ function HomeScreen({ navigation }) {
       </View>
       {/* SkillIndia */}
       <Skillindia />
-      <View style={{ color: "grey", height: 120 }}></View>
+      <View style={{ color: 'grey', height: 120 }}></View>
     </ScrollView>
-  );
+  )
 }
 
-export default HomeScreen;
+export default HomeScreen
